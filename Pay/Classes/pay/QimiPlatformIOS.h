9cb72f77f213c5de7ae12966b59713d802939917
benvim <benvim@gmail.com>
2013-06-26 13:39:34 +0800

    测试调试充值BUG，支付宝、直付通都可以充值

--- Pay/Classes/pay/QimiPlatformIOS.h
+++ Pay/Classes/pay/QimiPlatformIOS.h
@@ -25,15 +25,15 @@ public:
     void openWeb(std::string url);
     void openAlert(std::string title, std::string msg);
     
-    void alipayPay(std::string order,
-                   float price,
-                   std::string product,
-                   std::string productDes,
-                   std::string appScheme,
-                   std::string parent,
-                   std::string seller,
-                   std::string notifyUrl,
-                   std::string alipayKey);
+    void alipayPay(std::string order, //订单号
+                   float price, //支付金额
+                   std::string product, //产品名称
+                   std::string productDes, //产品描述
+                   std::string appScheme, //应用切换的scheme
+                   std::string parent, //商家支付宝帐号ID
+                   std::string seller, //商家支付宝帐号
+                   std::string notifyUrl, //支付成功的回调地址
+                   std::string alipayKey); //商家支付宝密钥
     
 private:
     
